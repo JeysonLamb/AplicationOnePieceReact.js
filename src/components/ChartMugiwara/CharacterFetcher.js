ecf648d0740101bd99fc7cf716d04ac61047a025
Jeyson Lamb <lambjeyson@gmail.com>
2024-10-15 18:52:51 -0500

    Arreglo pantallas de carga. Closes #3

--- src/components/ChartMugiwara/CharacterFetcher.js
+++ src/components/ChartMugiwara/CharacterFetcher.js
@@ -1,13 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import getCharacters from "../../services/apiService";
 import characterImages from "./chartersImages"; // Importa las imágenes de los personajes
-import './CharacterCard.css'; // Archivo CSS importado
 
 const CharacterFetcher = ({ setCharacters }) => {
-  // const [loading, setLoading] = useState(true); // Inicializa el estado de carga
+  const [loading, setLoading] = useState(true); // Inicializa el estado de carga
 
   useEffect(() => {
     const fetchCharacters = async () => {
+      setCharacters([]); // Resetear characters a un array vacío
+
       const data = await getCharacters();
       const filteredCharacters = data
         .filter(({ name }) => characterImages[name])
@@ -18,21 +19,22 @@ const CharacterFetcher = ({ setCharacters }) => {
           age,
           bounty,
         }));
+      console.log(data);
 
       setCharacters(filteredCharacters);
-      // setLoading(false); // Cambia a falso una vez cargados los personajes
+      setLoading(false); // Cambia a falso una vez cargados los personajes
     };
 
     fetchCharacters();
   }, [setCharacters]);
-  // if (loading) {
-  //   return (
-  //     <div className="loading">
-  //       <div className="spinner"></div>
-  //       <p className="charge"> Cargando personajes...</p>
-  //     </div>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <div className="loading">
+        <div className="spinner"></div>
+        <p className="charge"> Cargando personajes...</p>
+      </div>
+    );
+  }
   return null; // Este componente no necesita renderizar nada más
 };
 
