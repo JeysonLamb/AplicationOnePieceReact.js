00c8d38dfc3e5f5154f6ad49774fd7e16efd18c9
Jeyson Lamb <lambjeyson@gmail.com>
2024-10-17 12:30:21 -0500

    Renombrada carpeta de carta para mejor viabilidad

---
+++ src/components/CharacterCard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import carta from '../../Imagenes/carta.png'; // Imagen para la parte trasera de la carta
+const CharacterCard = ({ character }) => {
+
+  return (
+    <div className="card" >
+      <div className="card-inner">
+        <div className="card-front">
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+          <p>Edad: {character.age}</p>
+          <p>Bounty: {character.bounty}</p>
+        </div>
+        <div className="card-back">
+          <img src={carta} alt="Carta" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CharacterCard;
