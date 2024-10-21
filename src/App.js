94329227491e78ef36b31c5c55cb1b4e7b95f66e
Jeyson Lamb <lambjeyson@gmail.com>
2024-10-21 17:56:32 -0500

    Organiza rutas y ventanas para mejor legibilidad

--- src/App.js
+++ src/App.js
@@ -1,67 +1,35 @@
-import React from "react";
-import { Route, Switch, Link, Redirect } from "wouter";
-import CharacterFetcher from "./components/CharacterImagFetcher/Index";
+import React, { useState } from "react";
+import { Route, Switch} from "wouter";
 import Home from "./pages/Home/Index"; // Importa el componente Home
-import logo from "./Imagenes/Logo.png";
-import logoHeart from "./Imagenes/LogoHeart.png";
+import Mugiwaras from "./pages/CrewMugiwara/Index"; // Importa el componente Mugiwaras
+import PiratasHeart from "./pages/CrewHeart/Index"; // Importa el componente PiratasHeart
+import PiratasKid from "./pages/CrewKid/Index"; // Importa el componente PiratasKid
 import "./App.css";
 import "./components/CharacterImagFetcher/HeartStyle.css";
 import "./components/CharacterImagFetcher/KidStyle.css";
 
-import logoKid from "./Imagenes/ImagKid/logoKid.png";
-
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+
   return (
     <div>
       <Switch>
-        {/* Página principal */}
+        {/* Página principal que solo muestra los botones de navegación */}
         <Route path="/">
           <Home />
         </Route>
-
         {/* Ruta para ver la tripulación Mugiwara */}
         <Route path="/mugi">
-          <div className="Mugiwaras">
-            <Link to="/">
-              <img className="logo" src={logo} alt="logo" />
-            </Link>
-            <h1>
-              <strong>Tripulación Mugiwara</strong>
-            </h1>
-            <CharacterFetcher crew="mugiwara" />
-          </div>
+          <Mugiwaras setCharacters={setCharacters} characters={characters} />
         </Route>
-
         {/* Ruta para ver la tripulación Piratas Heart */}
         <Route path="/heart">
-          <div className="Heart">
-            <Link to="/">
-              <img className="logo" src={logoHeart} alt="logo" />
-            </Link>
-            <h1 className="titule">
-              <strong>Piratas Heart</strong>
-            </h1>
-            <CharacterFetcher crew="heart" />
-          </div>
+          <PiratasHeart setCharacters={setCharacters} characters={characters} />
         </Route>
-
         {/* Ruta para ver la tripulación de Kid */}
         <Route path="/kid">
-          <div className="kid">
-            <Link to="/">
-              <img className="LogoKid" src={logoKid} alt="logo" />
-            </Link>
-            <h1 className="titule">
-              <strong>Piratas de Kid</strong>
-            </h1>
-            <CharacterFetcher crew="kid" />
-          </div>
-        </Route>
-
-        {/* Redirección a Home si no existe la ruta */}
-        <Route>
-          <Redirect to="/" />
-        </Route>
+          <PiratasKid setCharacters={setCharacters} characters={characters} />
+        </Route>    
       </Switch>
     </div>
   );
