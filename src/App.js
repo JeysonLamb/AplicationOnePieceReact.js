4a8d769e4c3abe970bf5037a73a9faecda6f281c
Jeyson Lamb <lambjeyson@gmail.com>
2024-10-21 17:07:14 -0500

    optimiza app.js y se coloca el llamado de las cartas en CharacterImagFethcer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Switch, Link, Redirect } from "wouter";
-import CharacterCard from "./components/CharacterCard/index";
 import CharacterFetcher from "./components/CharacterImagFetcher/Index";
 import Home from "./pages/Home/Index"; // Importa el componente Home
 import logo from "./Imagenes/Logo.png";
@@ -10,13 +9,12 @@ import "./components/CharacterImagFetcher/HeartStyle.css";
 import "./components/CharacterImagFetcher/KidStyle.css";
 
 import logoKid from "./Imagenes/ImagKid/logoKid.png";
-const App = () => {
-  const [characters, setCharacters] = useState([]);
 
+const App = () => {
   return (
     <div>
       <Switch>
-        {/* Página principal que solo muestra los botones de navegación */}
+        {/* Página principal */}
         <Route path="/">
           <Home />
         </Route>
@@ -30,53 +28,33 @@ const App = () => {
             <h1>
               <strong>Tripulación Mugiwara</strong>
             </h1>
-            <CharacterFetcher setCharacters={setCharacters} crew="mugiwara" />
-            <ul>
-              {characters.map((char) => (
-                <li key={char.id}>
-                  <CharacterCard character={char} />
-                </li>
-              ))}
-            </ul>
+            <CharacterFetcher crew="mugiwara" />
           </div>
         </Route>
 
         {/* Ruta para ver la tripulación Piratas Heart */}
         <Route path="/heart">
-          <div className="Heart ">
+          <div className="Heart">
             <Link to="/">
               <img className="logo" src={logoHeart} alt="logo" />
             </Link>
             <h1 className="titule">
               <strong>Piratas Heart</strong>
             </h1>
-            <CharacterFetcher setCharacters={setCharacters} crew="heart" />
-            <ul>
-              {characters.map((char) => (
-                <li key={char.id}>
-                  <CharacterCard character={char} />
-                </li>
-              ))}
-            </ul>
+            <CharacterFetcher crew="heart" />
           </div>
         </Route>
 
+        {/* Ruta para ver la tripulación de Kid */}
         <Route path="/kid">
-          <div className="kid ">
+          <div className="kid">
             <Link to="/">
               <img className="LogoKid" src={logoKid} alt="logo" />
             </Link>
             <h1 className="titule">
-              <strong>Piratas de Kit</strong>
+              <strong>Piratas de Kid</strong>
             </h1>
-            <CharacterFetcher setCharacters={setCharacters} crew="kid" />
-            <ul>
-              {characters.map((char) => (
-                <li key={char.id}>
-                  <CharacterCard character={char} />
-                </li>
-              ))}
-            </ul>
+            <CharacterFetcher crew="kid" />
           </div>
         </Route>
 
