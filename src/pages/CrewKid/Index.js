018384b3a8c770a549821d2c7f5dcd6167ad3f34
Jeyson Lamb <lambjeyson@gmail.com>
2024-10-21 18:00:04 -0500

    Crea pages: Piratas Kid y llaman ruta al App.js

---
+++ src/pages/CrewKid/Index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "wouter";
+import CharacterFetcher from "../../components/CharacterImagFetcher/Index"; // Asegúrate de que la ruta sea correcta
+import logoKid from "../../Imagenes/ImagKid/logoKid.png"; // Ajusta la ruta de la imagen
+
+const PiratasKid = () => {
+  return (
+    <div className="kid">
+      <Link to="/">
+        <img className="LogoKid" src={logoKid} alt="logo" />
+      </Link>
+      <h1 className="titule">
+        <strong>Piratas de Kid</strong>
+      </h1>
+      <CharacterFetcher crew="kid" />
+    </div>
+  );
+};
+
+export default PiratasKid;
