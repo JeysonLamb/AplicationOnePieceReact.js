ade83fdde355651672c9c3c6969bd8997a8974ba
Jeyson Lamb <lambjeyson@gmail.com>
2024-10-23 11:17:54 -0500

    Envuelve la aplicación en GlobalProvider y reestructura las rutas.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import React, { useState } from "react";
-import { Route, Switch} from "wouter";
-import Home from "./pages/Home/Index"; // Importa el page Home
-import Mugiwaras from "./pages/CrewMugiwara/Index"; // Importa el page Mugiwaras
-import PiratasHeart from "./pages/CrewHeart/Index"; // Importa el page PiratasHeart
-import PiratasKid from "./pages/CrewKid/Index"; // Importa el page PiratasKid
+import React from "react";
+import { Route} from "wouter";
+import Home from "./pages/Home/Index"; 
+import Mugiwaras from "./pages/CrewMugiwara/Index"; 
+import PiratasHeart from "./pages/CrewHeart/Index"; 
+import PiratasKid from "./pages/CrewKid/Index"; 
+import { GlobalProvider } from "./context/GlobalContext"; 
 import "./App.css";
 import "./components/CharacterImagFetcher/HeartStyle.css";
 import "./components/CharacterImagFetcher/KidStyle.css";
 
 const App = () => {
-  const [characters, setCharacters] = useState([]);
-
   return (
-    <div>
-      <Switch>
-        {/* Página principal que solo muestra los botones de navegación */}
-        <Route path="/">
-          <Home />
-        </Route>
-        {/* Ruta para ver la tripulación Mugiwara */}
-        <Route path="/mugi">
-          <Mugiwaras setCharacters={setCharacters} characters={characters} />
-        </Route>
-        {/* Ruta para ver la tripulación Piratas Heart */}
-        <Route path="/heart">
-          <PiratasHeart setCharacters={setCharacters} characters={characters} />
-        </Route>
-        {/* Ruta para ver la tripulación de Kid */}
-        <Route path="/kid">
-          <PiratasKid setCharacters={setCharacters} characters={characters} />
-        </Route>    
-      </Switch>
-    </div>
+    <GlobalProvider> {/* Envolvemos toda la app en el GlobalProvider */}
+      <div>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/mugi">
+            <Mugiwaras />
+          </Route>
+          <Route path="/heart">
+            <PiratasHeart />
+          </Route>
+          <Route path="/kid">
+            <PiratasKid />
+          </Route>    
+      </div>
+    </GlobalProvider>
   );
 };
 
